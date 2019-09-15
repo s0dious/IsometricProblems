114d789ecdce46a552e6fd3cc99469a29522252b
Nathan Kulczak <nathan.kulczak@mail.missouri.edu>
2019-09-15 00:41:26 -0500

    Added an example draw

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // C/C++ Libraries
 #include <iostream>
 #include <string>
+#include <fstream>
 
 // SFML Libraries
 #include <SFML/Window.hpp>
@@ -11,6 +12,25 @@
 
 // Project files
 #include "shaders.hpp"
+
+std::string load_file(std::string filename)
+{
+    std::string file_string;
+
+    std::ifstream file(filename);
+    if(file.is_open())
+    {
+        std::string temp;
+        while(std::getline(file, temp))
+        {
+            file_string += temp + "\n";
+        }
+
+        // file_string += "\0";
+    }
+
+    return file_string;
+}
 
 int main()
 {
@@ -32,6 +52,83 @@ int main()
     gladLoadGL();
 
     // Load shaders
+    std::string vertex_shader_string = load_file(std::string("shaders/vertex.glsl"));
+    std::string fragment_shader_string = load_file(std::string("shaders/fragment.glsl"));
+
+    const GLchar* vertex_shader_source = vertex_shader_string.c_str();
+    const GLchar* fragment_shader_source = fragment_shader_string.c_str();
+
+    GLint success;
+    GLchar info_log[512];
+
+    // Load vertex shader and make sure it compiles
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << std::endl;
+    }
+
+    // Load fragment shader and make sure it compiles
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log << std::endl;
+    }
+
+    // Make a shader program, link shaders, and check for linking errors
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(shader_program, 512, NULL, info_log);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << std::endl;
+    }
+
+    // Define vertex and index data
+    GLfloat vertices[] = {
+        0.5f, 0.5f, 0.0f,   // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f   // top left
+    };
+
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // Create vertex array and buffer objects
+    GLuint vertex_array_object, vertex_buffer_object, element_buffer_object;
+
+    glGenVertexArrays(1, &vertex_array_object);
+    glGenBuffers(1, &vertex_buffer_object);
+    glGenBuffers(1, &element_buffer_object);
+
+    // Bind vertex array, buffers, and set attributes
+    glBindVertexArray(vertex_array_object);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_object);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
 
 
     // Event loop where all the magic happens
@@ -56,6 +153,11 @@ int main()
         // Clear screen
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw objects
+        glUseProgram(shader_program);
+        glBindVertexArray(vertex_array_object);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Push buffer to screen
         window.display();
